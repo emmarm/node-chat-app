2615f7eb41db183718c01b2834d09d8246073265
Emma M <mdopure@gmail.com>
2018-03-28 06:51:02 +0700

    Added emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Someone',
-    text: 'Long time no see!'
-  });
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Example',
-    text: 'Hey everyone',
-    createdAt: 1234567890987
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
