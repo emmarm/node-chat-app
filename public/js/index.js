2e9f1e2ae79f395dc1c2047483600ded45a809fc
Emma M <mdopure@gmail.com>
2018-03-28 19:44:47 +0700

    Added message form and display messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,18 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('New message', message);
+  const messageList = document.querySelector('#messages');
+  const listItem = document.createElement('li');
+  listItem.innerText = `${message.from}: ${message.text}`;
+
+  messageList.appendChild(listItem);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: document.querySelector('[name="message"]').value
+  }, (err) => console.log(err));
 });
